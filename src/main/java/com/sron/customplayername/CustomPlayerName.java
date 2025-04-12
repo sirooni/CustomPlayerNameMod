53036ba2b32de3c605e1d3890f935105494ca46f
sirooni <82398008+sirooni@users.noreply.github.com>
2025-04-12 18:12:09 +0900

    chore: コードスタイルの適用

--- src/main/java/com/sron/customplayername/CustomPlayerName.java
+++ src/main/java/com/sron/customplayername/CustomPlayerName.java
@@ -3,8 +3,10 @@ package com.sron.customplayername;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(CustomPlayerName.MODID)
-public class CustomPlayerName
-{
-    public static final String MODID = "customplayername";
-    public CustomPlayerName() {}
+public class CustomPlayerName {
+
+  public static final String MODID = "customplayername";
+
+  public CustomPlayerName() {
+  }
 }
