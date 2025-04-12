ddfdb2f3144c36ed37a649f5a5b1892cc69fa4e7
sirooni <82398008+sirooni@users.noreply.github.com>
2025-04-12 18:03:09 +0900

    chore: 雛形のコメントの削除

--- src/main/java/com/sron/customplayername/CustomPlayerName.java
+++ src/main/java/com/sron/customplayername/CustomPlayerName.java
@@ -2,7 +2,6 @@ package com.sron.customplayername;
 
 import net.minecraftforge.fml.common.Mod;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(CustomPlayerName.MODID)
 public class CustomPlayerName
 {
