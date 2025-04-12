fa7886dd07442080194dd8b6cbb0f5fae7e55f53
sirooni <82398008+sirooni@users.noreply.github.com>
2025-04-12 16:39:09 +0900

    chore: modid等の変更

---
+++ src/main/java/com/sron/customplayername/CustomPlayerName.java
@@ -0,0 +1,11 @@
+package com.sron.customplayername;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(CustomPlayerName.MODID)
+public class CustomPlayerName
+{
+    public static final String MODID = "customplayername";
+    public CustomPlayerName() {}
+}
